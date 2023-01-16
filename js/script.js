d059af4e8e76cb7b955cfa019736533d3a710894
Jakhongir611 <khurramovjahongir611@gmail.com>
2023-01-16 18:13:03 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let btn_menu = document.querySelector('.burger')
+let menu = document.querySelector('.wrap-top-right')
+let menu_x = document.querySelector('.burger_X')
+
+btn_menu.addEventListener('click', function() {
+    menu.classList.toggle('active')
+})
+menu_x.addEventListener('click', function() {
+    menu.classList.toggle('active')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn_menu.addEventListener('click', function()){
+//     btn_menu.classList.keys('active')
+// }
